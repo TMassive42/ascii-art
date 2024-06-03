55f53cad569b677b53babaaa1273f2f971409254
togondol <obimbira60@gmail.com>
2024-06-03 15:50:59 +0300

    created ascii-art program that converts input strings into graphic representations using ASCII characters

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"flag"
+
+	"ascii/ascii"
+)
+
+// formats is a map that maps output format flags to their corresponding filenames.
+var formats = map[string]string{
+	"s":   "standard.txt",
+	"t": "thinkertoy.txt",
+	"sh":     "shadow.txt",
+}
+
+func main() {
+	formatPtr := flag.String("f", "s", "Output format: s (standard), t (thinkertoy), sh (shadow)")
+	flag.Parse()
+
+	
+	
+	filename, ok := formats[*formatPtr]
+	if !ok {
+		fmt.Println("Invalid format specified.")
+		return
+	}
+
+	asciMap, err := ascii.ReadASCIIMapFromFile(filename)
+	if err != nil {
+		log.Fatalf("Error reading ASCII map: %v", err)
+	}
+
+	args := flag.Args()
+	if len(args) != 1 {
+		fmt.Println("Please provide only one input string.")
+		return
+	}
+
+	input := args[0]
+
+	err = ascii.PrintArt(input, asciMap)
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
+}
